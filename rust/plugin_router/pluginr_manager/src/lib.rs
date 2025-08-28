fb557873c7cd631e92da45c5e1b6a52342fde6fa
munch1182 <munch1182@qq.com>
2025-08-29 01:03:05 +0800

    plugin_router

--- rust/plugin_router/pluginr_manager/src/lib.rs
+++ rust/plugin_router/pluginr_manager/src/lib.rs
@@ -1,14 +1,64 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{
+    collections::HashMap,
+    path::{Path, PathBuf},
+    sync::{Arc, RwLock},
+};
+
+use async_trait::async_trait;
+use axum::Router;
+use libcommon::prelude::*;
+use libloading::Library;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PluginRoute {
+    pub path: String,
+    pub method: String,
+    pub handler: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PluginInfo {
+    pub name: String,
+    pub version: String,
+    pub routes: Vec<PluginRoute>,
+}
+
+#[async_trait]
+pub trait AxumPlugin: Send + Sync {
+    fn get_info(&self) -> PluginInfo;
+    fn register_router(&self, router: Router) -> Router;
+}
+pub struct PluginManager {
+    plugins: RwLock<HashMap<String, (Library, Arc<dyn AxumPlugin>)>>,
+    plugin_dir: PathBuf,
+}
+
+impl PluginManager {
+    pub fn new(plugin_dir: impl AsRef<Path>) -> Self {
+        Self {
+            plugins: RwLock::new(HashMap::new()),
+            plugin_dir: plugin_dir.as_ref().to_path_buf(),
+        }
+    }
+
+    pub fn load_plugin<P: AsRef<Path>>(&self, lib_path: P) -> Result<()> {
+        self.load_plugin_impl(lib_path.as_ref())
+    }
+
+    fn load_plugin_impl(&self, lib_path: &Path) -> Result<()> {
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use libcommon::prelude::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_manager() -> Result<()> {
+        PluginManager::new("");
+        Ok(())
     }
 }
